03928f79eb4c3cef1a032365f9fbfa055db2b206
MJ <mohamed.aref32@gmail.com>
2021-08-06 21:19:00 -0500

    finished styling sign in form and added prevent default to buttons

--- src/components/signUp/SignupScreen.js
+++ src/components/signUp/SignupScreen.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React from "react";
 import "./SignupScreen.css";
 
 function SignupScreen() {
+ const register = (e)=>{
+   e.preventDefault();
+ }
+
+ const signIn = (e)=>{
+   e.preventDefault();
+ }
+
   return (
-    <div className="singup__screen">
-      
+    <div className="signup__screen">
+      <form>
+        <h1>Sign In</h1>
+        <input placeholder="Email" type="email" />
+        <input placeholder="password" type="password" />
+        <button type="submit" onClick={signIn}>Sign In</button>
+        <h4>
+          <span className="signup__screen__gray">New To Netflix? </span>
+          <span className="singup__screen__link" onClick={register}>Sign Up now.</span>
+        </h4>
+      </form>
     </div>
-  )
+  );
 }
 
-export default SignupScreen
+export default SignupScreen;
